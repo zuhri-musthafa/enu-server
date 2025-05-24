af67135658020d989b490f8c304880210b4f4270
zuhri_musthafa <achmadzuhrimusthafa@gmail.com>
2025-05-24 19:40:19 +0700

    fix app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import os
 
 from . import create_app
-config_mode = os.getenv("CONFIG MODE", "development")
+config_mode = os.getenv("CONFIG MODE")
 app = create_app(config_mode)
 
 if __name__ == '__main__':
